e0efe97b55de7d086c018902aec57c78e1b8992e
cnajayi <112961125+cnajayi@users.noreply.github.com>
2025-04-15 23:49:56 -0400

    Delete cs489jewels25-1 directory

--- cs489jewels25-1/README.md
+++
@@ -1,2 +0,0 @@
-# cs489jewels25
-Match-3 Jewels type of game implementation for the course CS489 Game Dev (Spring 25) at Loyola University Maryland
--- cs489jewels25-1/src/Globals.lua
+++
@@ -1,5 +0,0 @@
-gameWidth = 640
-gameHeight = 480
-windowWidth, windowHeight = love.graphics.getDimensions()
-gameState = "start" 
-debugFlag = false 
